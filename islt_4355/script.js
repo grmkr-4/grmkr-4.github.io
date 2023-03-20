227e3d162f34f4e7c31bd4cf8a1c7283a934382c
Grayson Manalang Mueller <123035258+grmkr-4@users.noreply.github.com>
2023-03-19 20:39:56 -0500

    Update script.js

--- islt_4355/script.js
+++ islt_4355/script.js
@@ -11,7 +11,7 @@ function toggleMenu() {
         menu.classList.add("active");
         
         // adds the close (x) icon 
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+        toggle.querySelector("a").innerHTML = "<i class='fa-window-close'></i>";
     }
 }
 /* Event Listener */
